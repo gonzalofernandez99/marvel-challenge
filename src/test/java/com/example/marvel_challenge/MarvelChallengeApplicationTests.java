d1830de0c1bff5cb0cca470b1dad5c8fd26e113d
gonzalofernandez99 <fernandezgonzalo55@gmail.com>
2024-05-26 23:10:05 -0300

    chore: Add initial project files and configuration

---
+++ src/test/java/com/example/marvel_challenge/MarvelChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.marvel_challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarvelChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
