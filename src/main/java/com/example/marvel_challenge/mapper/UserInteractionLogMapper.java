85864be5f6ea7cddc3bbdb31ddac972c08e4bd9b
gonzalofernandez99 <fernandezgonzalo55@gmail.com>
2024-07-29 22:52:45 -0300

    feat: Add new entity classes for permissions and user interaction logs

---
+++ src/main/java/com/example/marvel_challenge/mapper/UserInteractionLogMapper.java
@@ -0,0 +1,31 @@
+package com.example.marvel_challenge.mapper;
+
+import com.example.marvel_challenge.dto.GetUserInteractionLogDto;
+import com.example.marvel_challenge.persistence.integration.marvel.entity.UserInteractionLog;
+
+/**
+ * La clase `UserInteractionLogMapper` proporciona un método para mapear una entidad `UserInteractionLog` a un objeto `GetUserInteractionLogDto`.
+ */
+public class UserInteractionLogMapper {
+
+    /**
+     * Convierte una entidad `UserInteractionLog` en un objeto `GetUserInteractionLogDto`.
+     *
+     * @param entity La entidad `UserInteractionLog` que se va a convertir.
+     * @return Un objeto `GetUserInteractionLogDto` que representa la información de interacción del usuario.
+     */
+    public static GetUserInteractionLogDto toDto(UserInteractionLog entity){
+
+        if(entity == null) return null;
+
+        return new GetUserInteractionLogDto(
+                entity.getId(),
+                entity.getUrl(),
+                entity.getHttpMethod(),
+                entity.getUsername(),
+                entity.getTimestamp(),
+                entity.getRemoteAddress()
+        );
+
+    }
+}
---
+++ src/main/java/com/example/marvel_challenge/persistence/integration/marvel/entity/Role.java
@@ -0,0 +1,64 @@
+package com.example.marvel_challenge.persistence.integration.marvel.entity;
+
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+
+@Entity
+public class Role implements GrantedAuthority {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private RoleEnum name;
+
+    @OneToMany(fetch = FetchType.EAGER, mappedBy = "role")
+    private List<GrantedPermission> permissions;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public RoleEnum getName() {
+        return name;
+    }
+
+    public void setName(RoleEnum name) {
+        this.name = name;
+    }
+
+    @Override
+    public String getAuthority() {
+        if(name == null) return null;
+
+        return "ROLE_" + name.name();
+    }
+
+    public List<GrantedPermission> getPermissions() {
+        return permissions;
+    }
+
+    public void setPermissions(List<GrantedPermission> permissions) {
+        this.permissions = permissions;
+    }
+
+    public static enum RoleEnum{
+        CUSTOMER, AUDITOR;
+    }
+
+}
